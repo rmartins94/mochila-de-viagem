f48751be70458de45b5aaf26611b54f434d1a5d3
Renato Martins <rmartins6694@gmail.com>
2022-10-20 13:34:26 -0300

    add localStorage in atividade-01.js

--- consolidando-conhecimento/js-consolidando/atividade-01.js
+++ consolidando-conhecimento/js-consolidando/atividade-01.js
@@ -4,7 +4,7 @@ const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
 
 itens.forEach((element) => {
-    console.log(element.nome, element.quantidade)
+    criaElemento(element)
 });
 
 
@@ -14,32 +14,55 @@ form.addEventListener("submit", (event) => {
     const nome = event.target.elements['nome'];
     const quantidade = event.target.elements['quantidade'];
 
-    criaElemento(nome.value, quantidade.value);
+    const existe = itens.find(element => element.nome === nome.value);
+
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    if (existe){
+        itemAtual.id = existe.id
+
+        atualizaElemento(itemAtual);
+
+        itens[existe.id] = itemAtual;
+
+    } else {
+        itemAtual.id = itens.length;
+
+        criaElemento(itemAtual);
+
+        itens.push(itemAtual)
+    }
+
+    criaElemento(itemAtual);
+
+    itens.push(itemAtual);
+    
+    localStorage.setItem("itens", JSON.stringify(itens));
 
     nome.value =""
     quantidade.value =""
 });
 
-function criaElemento (nome, quantidade) {
+function criaElemento (item) {
 
 const novoItem = document.createElement('li');
 novoItem.classList.add("item");
 
 const numeroItem = document.createElement('strong');
-numeroItem.innerHTML = quantidade;
+numeroItem.innerHTML = item.quantidade;
+numeroItem.dataset.id = item.id
 
 novoItem.appendChild(numeroItem);
-novoItem.innerHTML += nome;
+novoItem.innerHTML += item.nome;
 
 
 lista.appendChild(novoItem);
 
-const itemAtual = {
-    "nome": nome,
-    "quantidade": quantidade
 }
-itens.push(itemAtual);
 
-localStorage.setItem("itens", JSON.stringify(itens));
-
+function atualizaElemento (item) {
+    document.querySelector("[data-id='"=item.id+"']").innerHTML = item.quantidade
 }
